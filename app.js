381c50755c3ba1faf4d5805054ff83c66ff924e5
csongin <cheonsong37@gmail.com>
2022-09-30 21:15:06 +0900

    [add] 게시물 등록(생성) 엔드포인트

---
+++ app.js
@@ -0,0 +1,103 @@
+const users = [
+  {
+    id: 1,
+    name: "Rebekah Johnson",
+    email: "Glover12345@gmail.com",
+    password: "123qwe",
+  },
+  {
+    id: 2,
+    name: "Fabian Predovic",
+    email: "Connell29@gmail.com",
+    password: "password",
+  },
+];
+const posts = [
+  {
+    id: 1,
+    title: "간단한 HTTP API 개발 시작!",
+    content: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+    userId: 1,
+  },
+  {
+    id: 2,
+    title: "HTTP의 특성",
+    content: "Request/Response와 Stateless!!",
+    userId: 1,
+  },
+];
+const postView = [];
+
+const http = require('http');
+const server = http.createServer();
+
+const httpRequestListener = (request, response) => {
+  const { url, method } = request;
+  
+  if (method === 'POST') {
+    // 유저 회원가입 하기
+    if (url === '/users/signup') {
+      let body = '';
+
+      request.on('data', (data) => {
+        body += data;
+      });
+
+      request.on('end', () => {
+        const user = JSON.parse(body);
+        users.push({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          password: user.password,
+        });
+        response.writeHead(200, {'Content-Type' : 'application/json'});
+        response.end(JSON.stringify({'users' : users}));
+      });
+    }
+    // 게시글 등록하기
+    if (url === '/posts/register') {
+      let body ='';
+
+      request.on('data', (data) => {
+        body += data;
+      });
+
+      request.on('end', () => {
+        const post = JSON.parse(body);
+
+        posts.push({
+          id: post.id,
+          title: post.title,
+          content: post.content,
+          userId: post.userId,
+        });
+        response.writeHead(200, {'Content-Type' : 'application/json'});
+        response.end(JSON.stringify({'posts' : posts}));
+      });
+    }
+  } else if (method === 'GET') {
+    if (url === '/posts/postview') {
+      for(let i = 0; i < users.length; i++){
+        postView.push({
+          userId: users[i].id,
+          userName: users[i].name,
+          postingId: posts[i].id,
+          postingTitle: posts[i].title,
+          postingContent: posts[i].content,
+        });
+      }    
+    }
+    response.writeHead(200, {'Content-Type' : 'application/json'});
+    response.end(JSON.stringify({'data' : postView}));
+  }
+}
+
+server.on('request', httpRequestListener);
+
+const IP = '127.0.0.1';
+const PORT = 8000;
+
+server.listen(PORT, IP, function() {
+  console.log(`Listening to request on ip ${IP} & port ${PORT}`);
+})
